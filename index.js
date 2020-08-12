1e7690b86bf25b4c86713789f7edd5ce9c6a836c
imjchiang <jchiang296@gmail.com>
2020-08-12 09:42:53 -0700

    [INIT] initialize all, add packages, add cloudinary and implement it

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-var express = require('express');
-var ejsLayouts = require('express-ejs-layouts');
-var app = express();
+require("dotenv").config();
+const express = require('express');
+const ejsLayouts = require('express-ejs-layouts');
+const app = express();
+const multer = require("multer");
+const upload = multer({ dest: "./uploads" });
+const cloudinary = require("cloudinary");
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
+cloudinary.config(process.env.CLOUDINARY_URL);
 
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+app.post("/", upload.single("myFile"), function(req, res)
+{
+  cloudinary.uploader.upload(req.file.path, (result) =>
+  {
+    res.send(result);
+  })
 });
 
 app.listen(3000);
